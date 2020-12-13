48d2e71163f08bdacf8b9d5853034980c2990efc
xvishnevskix <xvishnevskix@mail.ru>
2020-12-13 19:01:05 +0300

    t

--- КурсовойПроект/Startup.cs
+++ КурсовойПроект/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,15 +13,18 @@ namespace КурсовойПроект
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
         public void ConfigureServices(IServiceCollection services)
         {
+            // поддержка контроллеров и представлений MVC
+            services.AddControllersWithViews()
+                //выставление совместимости с asp.net core 3.0
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        {   
+            //информация об ошибках
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -28,11 +32,14 @@ namespace КурсовойПроект
 
             app.UseRouting();
 
+                //подключение поддержки статичных файлов в приложении (css js и тд)
+                app.UseStaticFiles();
+            //регистрация нужных маршрутов или эндпоинтов
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
                 });
             });
         }
