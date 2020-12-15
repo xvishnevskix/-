e2bc71b4e4c2b7e5ead98e12f1a59adaf38e682e
xvishnevskix <xvishnevskix@mail.ru>
2020-12-15 15:54:54 +0300

    AppDbContext

--- КурсовойПроект/Domain/Entities/ServiceItem.cs
+++ КурсовойПроект/Domain/Entities/ServiceItem.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace КурсовойПроект.Domain.Entities
 {
-    public class ServiceItem
+    public class ServiceItem : EntityBase
+
     {
+        [Required(ErrorMessage = "Заполните название услуги")]
+        [Display(Name = "Название услуги")]
+        public override string Title { get; set; }
+
+        [Display(Name = "Краткое описание услуги")]
+        public override string Subtitle { get; set; }
+
+        [Display(Name = "Полное описание услуги")]
+        public override string Text{ get; set; }
+
     }
 }
