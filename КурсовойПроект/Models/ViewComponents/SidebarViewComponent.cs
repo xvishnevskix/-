731a8c5398c0a4459b6cb55ceeab175a2d43b523
xvishnevskix <xvishnevskix@mail.ru>
2020-12-24 13:46:22 +0300

    Sidebar

---
+++ КурсовойПроект/Models/ViewComponents/SidebarViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using КурсовойПроект.Domain;
+
+namespace КурсовойПроект.Models.ViewComponents
+{
+    public class SidebarViewComponent : ViewComponent
+    {
+        private readonly DataManager dataManager;
+
+        public SidebarViewComponent(DataManager dataManager)
+        {
+	        this.dataManager = dataManager;
+        }
+        
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult) View("Default", dataManager.ServiceItems.GetServiceItems()));
+        }
+    }
+}
